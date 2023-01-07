cb3d2f1b146ba0f39eead6a454d18c82bd98a9e8
mims <113519976+MimPMC@users.noreply.github.com>
2023-01-07 15:09:55 +0100

    test ts

---
+++ test.ts
@@ -0,0 +1,12 @@
+let greetings: string = "Hej!"
+
+console.log(greetings)
+
+let userId: number = 3344 
+console.log(userId)
+
+userId.toFixed()
+
+let isLoggedIn:boolean = false
+
+console.log(isLoggedIn.valueOf()) 
